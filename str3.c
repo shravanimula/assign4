35268584d329f452d9fa65f0b585525b46673071
shravanimula <98573934+shravanimula@users.noreply.github.com>
2022-03-04 19:54:47 +0530

    passing structure variable as argument

---
+++ str3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+struct student{
+	char name[20];
+	int rollno;
+	float marks;
+};
+void fun_variable(struct student s)
+{
+	printf("%s\t%d\t%f\n",s.name,s.rollno,s.marks);
+}
+int main()
+{
+	struct student s1={"aaaa",1,71};
+	struct student s2={"bbbbb",2,67};
+	fun_variable(s1);
+	fun_variable(s2);
+}
+
